55563283ded0c2f19623b9fc4140ad52efbe7d3d
Rizal Alfandi <rizalalfandi123456789@gmail.com>
2023-11-30 05:03:20 +0700

    add dark mode

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useHead({
+  title: "Zfood",
+  meta: [{ name: "description", content: "ZFood is a site for finding various food recipes" }],
+});
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
